02a3572079944771e2f43a0ff101d73de807ed16
Nikhil Padmawar <nikhilpadmawar@Nikhils-MacBook-Pro.local>
2020-04-15 14:47:56 +0530

    NIKHIL | SUDLUCK : MODIFIED ENUM TO RETURN VALUES

--- src/main/java/MoveType.java
+++ src/main/java/MoveType.java
@@ -1,4 +1,18 @@
 public enum MoveType {
-    COOPERATE,
-    CHEAT
+    COOPERATE("CO"),
+    CHEAT("CH");
+
+    private String input;
+
+    MoveType(String input) {
+
+        this.input = input;
+    }
+
+    public static MoveType valueFor(String input) {
+        for (MoveType moveType : MoveType.values()) {
+            return moveType.input.equals(input) ? moveType : null;
+        }
+        return null;
+    }
 }
--- src/test/java/MoveTypeTest.java
+++ src/test/java/MoveTypeTest.java
@@ -2,10 +2,24 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+
 public class MoveTypeTest {
 
     @Test
     public void shouldReturnCooperateWhenInputIsCO() {
+        MoveType result = MoveType.valueFor("CO");
+        assertEquals(MoveType.COOPERATE,result);
+    }
 
+    @Test
+    public void shouldReturnCheatWhenInputIsCH() {
+        MoveType result = MoveType.valueFor("CH");
+        assertEquals(MoveType.CHEAT, result);
+    }
+
+    @Test
+    public void shouldReturnNullWhenInputIsDifferent() {
+        MoveType result = MoveType.valueFor("CHE");
+        assertEquals(null, result);
     }
 }
